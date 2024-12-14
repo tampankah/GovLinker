8e35b1f74ebcee715b5f493da468b4f19d55a9de
Karol <karolmalicki.001@gmail.com>
2024-12-15 00:27:19 +0100

    links open

--- app/government_assisntant/lib/widgets/chat_bubble.dart
+++ app/government_assisntant/lib/widgets/chat_bubble.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:html2md/html2md.dart' as html2md; // For converting HTML to Markdown
 
 class ChatBubble extends StatelessWidget {
   final String message;
@@ -13,8 +15,25 @@ class ChatBubble extends StatelessWidget {
     this.isMarkdown = false,
   }) : super(key: key);
 
+  // Function to open links
+  Future<void> _openLink(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Convert HTML to Markdown
+  String _convertHtmlToMarkdown(String html) {
+    return html2md.convert(html); // Uses html2md to convert HTML to Markdown
+  }
+
   @override
   Widget build(BuildContext context) {
+    final String renderedMessage = isMarkdown ? message : _convertHtmlToMarkdown(message);
+
     return Align(
       alignment: isUserMessage ? Alignment.centerRight : Alignment.centerLeft,
       child: Padding(
@@ -26,17 +45,17 @@ class ChatBubble extends StatelessWidget {
             border: Border.all(color: Colors.deepPurple, width: 2),
           ),
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: isMarkdown
-              ? MarkdownBody(
-            data: message,
+          child: MarkdownBody(
+            data: renderedMessage,
             styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
               p: const TextStyle(color: Colors.white),
               listBullet: const TextStyle(color: Colors.white),
             ),
-          )
-              : Text(
-            message,
-            style: const TextStyle(color: Colors.white),
+            onTapLink: (text, href, title) {
+              if (href != null) {
+                _openLink(href);
+              }
+            },
           ),
         ),
       ),
