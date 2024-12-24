6e6b35bc6b6dc40eba536a5e4eb1b2f34471112b
Karol <karolmalicki.001@gmail.com>
2024-12-24 07:45:22 +0100

    created deployment and infra

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,21 +5,3 @@ resource "google_artifact_registry_repository" "docker_repo" {
   location     = var.region
   description  = "Docker repository for storing container images"
 }
-
-resource "google_cloud_run_service" "api_service" {
-  name     = var.service_name
-  location = var.region
-
-  template {
-    spec {
-      containers {
-        image = "gcr.io/google-containers/pause:3.1" # Tymczasowy obraz (minimalny).
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
