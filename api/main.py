d2f00e6a5dd99aee8406080826bedef6e1f49de8
Karol <karolmalicki.001@gmail.com>
2024-12-25 15:37:10 +0100

    created Google Cloud Run for Flutter web

--- api/main.py
+++ api/main.py
@@ -7,16 +7,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# Configure CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 app.include_router(router)
 
 @app.get("/")
