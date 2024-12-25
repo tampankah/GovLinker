38f0fa3a83c8748822e83b40a850a7bd52319d86
Karol <karolmalicki.001@gmail.com>
2024-12-25 15:37:58 +0100

    created Google Cloud Run for Flutter web

--- api/main.py
+++ api/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 
 app = FastAPI(
     title="DMV Document Validator and Assistant",
     description="A combined API for document validation and DMV assistance.",
     version="1.0.0",
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://flutter-web-app-183025368636.us-central1.run.app",  # Adres URL Twojej aplikacji Flutter Web
+        "https://government-assistant-api-183025368636.us-central1.run.app"  # Adres URL Twojego API (opcjonalnie)
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
